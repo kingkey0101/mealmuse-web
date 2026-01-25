c81721c934a48bc24643f6291e982dce4e71b131
Keylan King <kingkey0101@outlook.com>
2026-01-25 14:12:32 -0600

    Added/ /lib/db.ts

---
+++ lib/db.ts
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb'
+import { Gloock } from 'next/font/google';
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient | null = null;
+let clientPromise: Promise<MongoClient>
+
+if (!uri) {
+    throw new Error("Please add MONGODB_URI to your .env.local");
+}
+
+if (process.env.NODE_ENV === "development") {
+    //@ts-ignore
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options);
+        // @ts-ignore
+        global._mongoClientPromise = client.connect();
+    }
+    // @ts-ignore
+    clientPromise = global._mongoClientPromise;
+} else {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+}
+
+export default clientPromise;
