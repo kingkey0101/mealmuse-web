733c0a99469c44e6d31c261ad0ad9e657ab026ff
Keylan King <kingkey0101@outlook.com>
2026-01-25 14:56:42 -0600

    Added/ lib/auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,53 @@
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from 'bcrypt'
+import clientPromise from './db'
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'text' },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials.password) {
+                    return null;
+                }
+
+                const client = await clientPromise;
+                const db = client.db();
+                const users = db.collection("users");
+                const user = await users.findOne({ email: credentials.email });
+
+                if (!user) return null
+                return {
+                    id: user._id.toString(),
+                    email: user.email,
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: 'jwt'
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = (user as any).id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user && token.id) {
+                (session.user as any).id = token.id;
+            }
+            return session;
+        }
+    },
+    pages: {
+        signIn: '/auth/login',
+    }
+}
+
